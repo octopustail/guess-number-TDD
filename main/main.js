11bb034815d91183970deb97fad0efdb0803d5c4
octopustail <494479914@qq.com>
2018-04-16 20:44:54 +0800

    使用TDD的方法完成了练习

--- main/main.js
+++ main/main.js
@@ -1,6 +1,34 @@
 // Write your cade below:
-function main() {};
-
-module.exports = {
-    main
+function geneRamdom() {
+    return Math.floor(Math.random() * 10001).toString();
 }
+function calAcount(srcStr, desStr) {
+    var aCount = 0;
+    for (var i = 0; i < srcStr.length; i++) {
+        if (srcStr[i] === desStr[i]) {
+            aCount++;
+        }
+    }
+    return aCount;
+}
+
+function calBcount(srcStr, desStr) {
+    var bCount = 0;
+    for (var i = 0; i < srcStr.length; i++) {
+        if ((srcStr[i] !== desStr[i]) && (desStr.indexOf(srcStr[i]) !== -1)) {
+            bCount++;
+        }
+    }
+    return bCount;
+}
+
+
+module.exports = function main(numStr) {
+    var aCount, bCount;
+    var goalStr = geneRamdom();
+    aCount = calAcount(numStr, goalStr);
+    bCount = calBcount(numStr, goalStr);
+
+    console.log('numStr',numStr,'goalStr',goalStr,'result',aCount+'A' + bCount+'B')
+    return aCount+'A' + bCount+'B';
+};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,24 @@
 const main = require('../main/main');
 
-describe('main()', () => {
+describe('main()', function () {
+
+    it('should print correct nA', function () {
+        var numA = '3A0B',
+            userinput = '1234',
+            result = main(userinput);
+        expect(result).toEqual(numA)
+    });
+    it('should print correct nB', function () {
+        var numB = '0A3B',
+            userinput = '5341',
+            result = main(userinput);
+        expect(result).toEqual(numB)
+    });
+    it('should print correct nAnB',function () {
+        var numC = '1A3B',
+            userinput = '1523',
+            result = main(userinput);
+        expect(result).toEqual(numC)
+    });
 
 });
